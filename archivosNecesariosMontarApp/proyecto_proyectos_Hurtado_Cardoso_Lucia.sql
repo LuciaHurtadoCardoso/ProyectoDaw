8435985ebc50c759a01903baafe506de46ef6c26
Lucía Hurtado Cardoso <57091651+LuciaHurtadoCardoso@users.noreply.github.com>
2020-06-19 20:28:33 +0200

    Subiendo archivos finales

--- archivosNecesariosMontarApp/proyecto_proyectos_Hurtado_Cardoso_Lucia.sql
+++ archivosNecesariosMontarApp/proyecto_proyectos_Hurtado_Cardoso_Lucia.sql
@@ -26,8 +26,8 @@ DROP TABLE IF EXISTS `admin`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -51,15 +51,15 @@ DROP TABLE IF EXISTS `alumno`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alumno` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) CHARACTER SET utf8 NOT NULL,
-  `apellidos` varchar(45) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `nombre` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `apellidos` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `enabled` tinyint(1) DEFAULT NULL,
-  `foto` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `foto` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +68,7 @@ CREATE TABLE `alumno` (
 
 LOCK TABLES `alumno` WRITE;
 /*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
-INSERT INTO `alumno` VALUES (1,'Lucía','Hurtado Cardoso','lucia@gmail.com','$2a$10$V2I9ykstviyOgyRn5gf5aeSHb7ET710Ie7Vx4L/1hp0WzjqBQgFn6',1,'Lucía_Hurtado_Cardoso_fotoPerfil_lucia.jpg'),(2,'Juan Ramon','Garcia Carrasco','juanra@gmail.com','$2a$10$dJK6fbeAtMND1p1ddAgZ8uJR9Gcxeb8HQf/nv/WhkCrkIphWsadmS',1,'92e79897-3878-43d3-834f-a80d238e01c0_IMG_3046.JPG'),(45,'lucia','hurtado','luciahurtadocardoso1@gmail.com','$2a$10$usaVktVQwYbRFI6TjJ6/quszdWfTEXbA3HO8WaczpXz.bv.WfsCsO',1,NULL);
+INSERT INTO `alumno` VALUES (57,'Lucía','Hurtado Cardoso','luciahurtadocardoso1@gmail.com','$2a$10$V2I9ykstviyOgyRn5gf5aeSHb7ET710Ie7Vx4L/1hp0WzjqBQgFn6',1,'Lucía_Hurtado_Cardoso_fotoPerfil_lucia.jpg'),(58,'Manuel','López Ramos','manuellp87@hotmail.com','$2a$10$V2I9ykstviyOgyRn5gf5aeSHb7ET710Ie7Vx4L/1hp0WzjqBQgFn6',1,'Manuel_López_Ramos_fotoPerfil_pp.jpg'),(59,'Cristina','Sierra Segura','cocosierrasegura@gmail.com','$2a$10$V2I9ykstviyOgyRn5gf5aeSHb7ET710Ie7Vx4L/1hp0WzjqBQgFn6',1,'Cristina_Sierra_Segura_fotoPerfil_Screenshot_1.jpg'),(60,'Juan Ramón','García Carrasco','juanra.gc94@gmail.com','$2a$10$V2I9ykstviyOgyRn5gf5aeSHb7ET710Ie7Vx4L/1hp0WzjqBQgFn6',1,'Juan Ramón_García_Carrasco_fotoPerfil_juanra.jpg');
 /*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,8 +106,8 @@ DROP TABLE IF EXISTS `codigo`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `codigo` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `rol` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `codigo` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `rol` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `codigo` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -118,7 +118,7 @@ CREATE TABLE `codigo` (
 
 LOCK TABLES `codigo` WRITE;
 /*!40000 ALTER TABLE `codigo` DISABLE KEYS */;
-INSERT INTO `codigo` VALUES (1,'profesor','soyElProfesor'),(2,'alumno','hola');
+INSERT INTO `codigo` VALUES (1,'profesor','soyElProfesor'),(2,'alumno','soyAlumnoDelIESFcoRomeroVargas');
 /*!40000 ALTER TABLE `codigo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,15 +131,15 @@ DROP TABLE IF EXISTS `comentario`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comentario` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `texto` varchar(255) COLLATE utf8_bin NOT NULL,
-  `role` varchar(45) COLLATE utf8_bin NOT NULL,
+  `texto` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `role` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `id_usuario` int NOT NULL,
   `fecha` date NOT NULL,
   `publicacion_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_comentario_publicacion1_idx` (`publicacion_id`),
   CONSTRAINT `fk_comentario_publicacion1` FOREIGN KEY (`publicacion_id`) REFERENCES `publicacion` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,7 +148,7 @@ CREATE TABLE `comentario` (
 
 LOCK TABLES `comentario` WRITE;
 /*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
-INSERT INTO `comentario` VALUES (20,'Testeando Comentarios','alumno',1,'2020-05-25',43),(21,'Testeando comentarios 2','alumno',1,'2020-05-25',43),(22,'Hola Lucía','profesor',1,'2020-05-25',43),(23,'asdsad','alumno',1,'2020-06-05',43);
+INSERT INTO `comentario` VALUES (28,'¿Te parece bien Juana?','alumno',57,'2020-06-19',49),(29,'¿Crees que debería cambiar algo?','alumno',57,'2020-06-19',49);
 /*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,7 +161,7 @@ DROP TABLE IF EXISTS `convocatoria`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `convocatoria` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `mes` varchar(100) COLLATE utf8_bin NOT NULL,
+  `mes` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -193,7 +193,7 @@ CREATE TABLE `profesor` (
   `foto` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +202,7 @@ CREATE TABLE `profesor` (
 
 LOCK TABLES `profesor` WRITE;
 /*!40000 ALTER TABLE `profesor` DISABLE KEYS */;
-INSERT INTO `profesor` VALUES (1,'Juana','Sánchez Rey','juana@gmail.com','$2a$10$OuQPFjPxYK74SNGOgwCLee.afLFaYw/d9wf0n81fYwXaXcClU5R9C',1,NULL),(2,'Pepe','Palo','pepe@gmail.com','$2a$10$2Q1ynlgMDei6fxFRhidrbOKiC3Aw/t//5D3aO9DQzIsczE8SeKRrK',1,'');
+INSERT INTO `profesor` VALUES (10,'Juana','Sánchez Rey','juana@gmail.com','$2a$10$V2I9ykstviyOgyRn5gf5aeSHb7ET710Ie7Vx4L/1hp0WzjqBQgFn6',1,'Juana_Sánchez_Rey_fotoPerfil_pp(1).jpg'),(11,'Paco','Ávila','paco@gmail.com','$2a$10$V2I9ykstviyOgyRn5gf5aeSHb7ET710Ie7Vx4L/1hp0WzjqBQgFn6',1,'Paco_Ávila_fotoPerfil_WhatsAppImage2020-06-18at17.32.53.jpeg');
 /*!40000 ALTER TABLE `profesor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -233,7 +233,7 @@ CREATE TABLE `proyecto` (
   CONSTRAINT `fk_proyecto_alumno1` FOREIGN KEY (`alumno_id`) REFERENCES `alumno` (`id`),
   CONSTRAINT `fk_proyecto_ciclo1` FOREIGN KEY (`ciclo_siglas`) REFERENCES `ciclo` (`siglas`),
   CONSTRAINT `fk_proyecto_profesor1` FOREIGN KEY (`profesor_id`) REFERENCES `profesor` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -242,7 +242,7 @@ CREATE TABLE `proyecto` (
 
 LOCK TABLES `proyecto` WRITE;
 /*!40000 ALTER TABLE `proyecto` DISABLE KEYS */;
-INSERT INTO `proyecto` VALUES (4,'test2',200,'texto',5,1,'hola',2019,2,NULL,'DAW','Junio'),(41,'Proyecto de proyectos',200,'La idea principal del proyecto se basa en algo parecido a una Moodle, pero destinada a la gestión de proyectos de fin de grado y a mejorar la comunicación entre alumno y profesor durante el desarrollo de estos.',10,1,'https://github.com/LuciaHurtadoCardoso/ProyectoDaw',2020,1,1,'DAW','Junio'),(59,'test',NULL,'hola',NULL,1,'hola',2020,2,NULL,'ASIR','Marzo');
+INSERT INTO `proyecto` VALUES (61,'Proyecto de proyectos',NULL,'La finalidad de mi proyecto, no es otra que, facilitar la comunicación entre alumno y profesor durante el seguimiento del proyecto y dar la posibilidad a los profesores de llevar un seguimiento más exhaustivo de los proyectos de forma sencilla.\nAdemás, esta aplicación servirá como almacén de antiguos proyectos, de los cuales los alumnos podrán coger ideas.',NULL,1,'https://github.com/LuciaHurtadoCardoso/ProyectoDaw',2020,57,10,'DAW','Junio'),(62,'Best Learning Resources',NULL,'Aplicación web que reune los mejores materiales para el aprendizaje de tecnologías.',NULL,1,'https://github.com/manu-lopez/proyectoDAW',2020,58,11,'DAW','Junio'),(63,'Moodle Académica',NULL,'CMS personalizado para gestionar alumn@s, cursos, temarios y exámenes por parte del profesor, y la realización de exámenes por parte del alumnado.',NULL,1,'https://github.com/piltrafillaeme/ProyectoDaw',2020,59,10,'DAW','Junio'),(64,'Chabrar',100,'Juego de preguntas por niveles en el que se evalúa la sabiduría de la persona en base a unos temas predeterminados.',10,0,'https://github.com/JuanraGC',2019,60,10,'DAW','Marzo');
 /*!40000 ALTER TABLE `proyecto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -268,7 +268,7 @@ CREATE TABLE `proyecto_finalizado` (
   `archivo` varchar(255) DEFAULT NULL,
   `mostrado` tinyint NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -277,7 +277,7 @@ CREATE TABLE `proyecto_finalizado` (
 
 LOCK TABLES `proyecto_finalizado` WRITE;
 /*!40000 ALTER TABLE `proyecto_finalizado` DISABLE KEYS */;
-INSERT INTO `proyecto_finalizado` VALUES (20,'hola','texto','test2',2019,'HTML5,MariaDB,CSS,SASS','DAW','Garcia Carrasco Juan Ramon','García Hurtado Juana',200,'Junio','2019_Junio_Garcia_Carrasco_Juan_Ramon_test2_final.zip',1),(24,'https://github.com/LuciaHurtadoCardoso/ProyectoDaw','La idea principal del proyecto se basa en algo parecido a una Moodle, pero destinada a la gestión de proyectos de fin de grado y a mejorar la comunicación entre alumno y profesor durante el desarrollo de estos.','Proyecto de proyectos',2020,'HTML5,CSS,JWT,SASS,Angular,JavaScript,SQL','DAW','Hurtado Cardoso Lucía','Sánchez Rey Juana',200,'Junio','2020_Junio_Hurtado_Cardoso_Lucía_Proyecto_de_proyectos_final.zip',0);
+INSERT INTO `proyecto_finalizado` VALUES (28,'https://github.com/JuanraGC','Juego de preguntas por niveles en el que se evalúa la sabiduría de la persona en base a unos temas predeterminados.','Chabrar',2019,'CSS,JavaScript,HTML5','DAW','García Carrasco Juan Ramón','Sánchez Rey Juana',100,'Marzo',NULL,1);
 /*!40000 ALTER TABLE `proyecto_finalizado` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -297,7 +297,7 @@ CREATE TABLE `publicacion` (
   PRIMARY KEY (`id`,`proyecto_id`),
   KEY `fk_publicacion_proyecto1_idx` (`proyecto_id`),
   CONSTRAINT `fk_publicacion_proyecto1` FOREIGN KEY (`proyecto_id`) REFERENCES `proyecto` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -306,7 +306,7 @@ CREATE TABLE `publicacion` (
 
 LOCK TABLES `publicacion` WRITE;
 /*!40000 ALTER TABLE `publicacion` DISABLE KEYS */;
-INSERT INTO `publicacion` VALUES (43,'2020-05-25_Hurtado_Cardoso_Lucía_GNY323V8CN.pdf','2020-05-25','Comprobación',41),(45,'2020-05-25_Garcia_Carrasco_Juan_Ramon_CV.pdf','2020-05-25','Hola que tal',4),(46,'2020-05-25_Garcia_Carrasco_Juan_Ramon_fotoMama.PNG','2020-05-25','Comprobaciones',4),(47,NULL,'2020-06-05','testeando comentarios',41);
+INSERT INTO `publicacion` VALUES (49,'2020-06-19_Hurtado_Cardoso_Lucía_documentacion_Hurtado_Cardoso_Lucia.pdf','2020-06-19','Documentación del proyecto',61),(50,'2020-06-19_Hurtado_Cardoso_Lucía_guia_de_estilos_Hurtado_Cardoso_Lucia.pdf','2020-06-19','Guía de estilos del proyecto',61),(51,'2020-06-19_Hurtado_Cardoso_Lucía_EntidadRelacion_Hurtado_Cardoso_Lucia.jpg','2020-06-19','Modelo entidad relación de la base de datos',61),(52,'2020-06-19_Hurtado_Cardoso_Lucía_seo_Hurtado_Cardoso_Lucia.pdf','2020-06-19','SEO de la aplicación',61),(53,'2020-06-19_López_Ramos_Manuel_EntidadRelacion_Hurtado_Cardoso_Lucia.jpg','2020-06-19','Updateando modelo relacional de la base de datos.',62),(54,'2020-06-19_Sierra_Segura_Cristina_proyecto_proyectos_Hurtado_Cardoso_Lucia.sql','2020-06-19','Juana, ya tengo listo el .sql de la base de datos.',63);
 /*!40000 ALTER TABLE `publicacion` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -321,7 +321,7 @@ CREATE TABLE `tag` (
   `id` int NOT NULL AUTO_INCREMENT,
   `tag` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -330,7 +330,7 @@ CREATE TABLE `tag` (
 
 LOCK TABLES `tag` WRITE;
 /*!40000 ALTER TABLE `tag` DISABLE KEYS */;
-INSERT INTO `tag` VALUES (1,'HTML5'),(2,'CSS'),(3,'SASS'),(4,'JAVA'),(5,'Sping5'),(6,'JavaScript'),(7,'JQuery'),(8,'Angular'),(9,'Python'),(10,'PHP'),(11,'SQL'),(12,'MariaDB'),(68,'JWT');
+INSERT INTO `tag` VALUES (1,'HTML5'),(2,'CSS'),(3,'SASS'),(4,'JAVA'),(5,'Sping5'),(6,'JavaScript'),(7,'JQuery'),(8,'Angular'),(9,'Python'),(10,'PHP'),(11,'SQL'),(12,'MariaDB'),(68,'JWT'),(83,'Django'),(84,'Docker'),(85,'Laravel');
 /*!40000 ALTER TABLE `tag` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -358,7 +358,7 @@ CREATE TABLE `tag_etiqueta_proyecto` (
 
 LOCK TABLES `tag_etiqueta_proyecto` WRITE;
 /*!40000 ALTER TABLE `tag_etiqueta_proyecto` DISABLE KEYS */;
-INSERT INTO `tag_etiqueta_proyecto` VALUES (1,4),(2,4),(3,4),(12,4),(1,41),(2,41),(3,41),(6,41),(8,41),(11,41),(68,41);
+INSERT INTO `tag_etiqueta_proyecto` VALUES (1,61),(2,61),(4,61),(5,61),(6,61),(7,61),(8,61),(12,61),(68,61),(83,62),(84,62),(6,63),(10,63),(85,63),(1,64),(2,64),(6,64);
 /*!40000 ALTER TABLE `tag_etiqueta_proyecto` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -371,4 +371,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-17 18:07:09
+-- Dump completed on 2020-06-19 20:14:29
